0095a9c0b66b7c5dc5e4182ce881ada22c730b9b
hong-sa-joon <mek125@naver.com>
2024-02-07 11:25:18 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+//        main 작성후 엔터 ↓ 코드작성
+    public static void main(String[] args){
+/*        내용은 main 안에 작성
+        System.out.println("안녕 자바!");
+        System.out.println("나는 홍사준이라고 해");
+문자열(줄바꿈 println)을 찍고싶으면 ↑ 코드 작성
+        System.out.print("안녕 자바 !");
+        System.out.print("나는 홍사준이라고 해");
+문자열(한줄로 print)을 찍고싶으면 ↑ 코드 작성
+ */
+
+        System.out.println("안녕자바");
+//sout을 치고 엔터 ↑ 코드 작성
+
+    }
+}
---
+++ src/NewMain.java
@@ -0,0 +1,5 @@
+public class NewMain {
+    public static void main(String[] args) {
+        System.out.println("홍사준입니다");
+    }
+}
